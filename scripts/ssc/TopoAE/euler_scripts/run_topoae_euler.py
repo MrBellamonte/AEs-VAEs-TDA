f13e1a625d781447c6d1956b9cd22818cbaaddd3
MrBellamonte <schsimo@ethz.ch>
2020-07-28 16:43:12 +0200

    TopoAe read to run

--- scripts/ssc/TopoAE/euler_scripts/run_topoae_euler.py
+++ scripts/ssc/TopoAE/euler_scripts/run_topoae_euler.py
@@ -3,4 +3,4 @@ from src.models.TopoAE.train_engine import simulator_TopoAE
 
 if __name__ == "__main__":
     path = '/cluster/home/schsimo/MT/output/280720'
-    simulator_TopoAE(eulergrid_280720,path,verbose= True, data_constant=True)
+    simulator_TopoAE(eulergrid_280720_2,path,verbose= True, data_constant=True)
--- scripts/ssc/TopoAE/topoae_config_library.py
+++ scripts/ssc/TopoAE/topoae_config_library.py
@@ -6,7 +6,7 @@ from src.models.autoencoders import Autoencoder_MLP_topoae
 
 test_grid = ConfigGrid_TopoAE(
     learning_rate=[1/1000],
-    batch_size=[512],
+    batch_size=[10000],
     n_epochs=[2],
     rec_loss_weight=[1],
     top_loss_weight=[1],
@@ -26,10 +26,30 @@ test_grid = ConfigGrid_TopoAE(
 ### All Models run on Euler
 
 
+## FIRST RUNS
 # regular batch sizes
 eulergrid_280720 = ConfigGrid_TopoAE(
     learning_rate=[1/1000],
     batch_size=[int(i) for i in np.logspace(4,9,num=6,base = 2.0)],
+    n_epochs=[40],
+    rec_loss_weight=[1],
+    top_loss_weight=[1/4,1/2,1,2,4],
+    model_class=[Autoencoder_MLP_topoae],
+    model_kwargs={
+        'input_dim'         : [101],
+        'latent_dim'        : [2],
+        'size_hidden_layers': [[128, 64, 32]]
+    },
+    dataset=[Spheres()],
+    sampling_kwargs={
+        'n_samples': [500]
+    }
+)
+
+# medium batch sizes
+eulergrid_280720_2 = ConfigGrid_TopoAE(
+    learning_rate=[1/1000],
+    batch_size=[int(i) for i in np.logspace(10,12,num=3,base = 2.0)],
     n_epochs=[40],
     rec_loss_weight=[1],
     top_loss_weight=[1/4,1/2,1,2,4],
