5fbf5f7e0d0d1ac35fa04e64a1ff7219822df968
MrBellamonte <schsimo@ethz.ch>
2020-08-11 20:12:57 +0200

    set configurations for cluster parallel

--- scripts/ssc/TopoAE/euler_scripts/run_topoae_euler_parallel_spheres.py
+++ scripts/ssc/TopoAE/euler_scripts/run_topoae_euler_parallel_spheres.py
@@ -3,4 +3,4 @@ from src.models.TopoAE.train_engine import simulator_TopoAE
 from joblib import Parallel, delayed
 
 if __name__ == "__main__":
-    Parallel(n_jobs=9)(delayed(simulator_TopoAE)(config) for config in spheres_euler_seed6_parallel_shuffled)
+    Parallel(n_jobs=13)(delayed(simulator_TopoAE)(config) for config in spheres_euler_seed6_parallel_shuffled)
