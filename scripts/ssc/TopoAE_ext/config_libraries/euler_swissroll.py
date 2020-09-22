a1d9a79d927b4f54fae56cc6187a82c0fe6f8851
MrBellamonte <schsimo@ethz.ch>
2020-09-22 15:59:34 +0200

    Experiments Euler

--- scripts/ssc/TopoAE_ext/config_libraries/euler_swissroll.py
+++ scripts/ssc/TopoAE_ext/config_libraries/euler_swissroll.py
@@ -265,3 +265,48 @@ asymapush_grid2 = [ConfigGrid_TopoAE_ext(
     num_threads=1,
     verbose = False,
 ) for mu_push, seed in zip(list(np.repeat([1.05,1.1,1.15],5)),[3859, 2525, 2068, 3302, 2517]*3)]
+
+
+# CHECK STABILITY OF METHOD
+
+asymapush_stability = [ConfigGrid_TopoAE_ext(
+    learning_rate=[1/25],
+    batch_size=[128],
+    n_epochs=[1000],
+    weight_decay=[1e-7],
+    early_stopping=[50],
+    rec_loss_weight=[1],
+    top_loss_weight=[8192],
+    match_edges = ['push_active'],
+    k = [2],
+    r_max = [10],
+    model_class=[Autoencoder_MLP_topoae],
+    model_kwargs={
+        'input_dim'         : [3],
+        'latent_dim'        : [2],
+        'size_hidden_layers': [[32, 32]]
+    },
+    dataset=[SwissRoll()],
+    sampling_kwargs={
+        'n_samples': [2560],
+        'seed' : [32, 539, 934, 331, 517]
+    },
+    eval=[ConfigEval(
+        active = True,
+        evaluate_on = 'test',
+        save_eval_latent = True,
+        save_train_latent = True,
+        online_visualization = False,
+        k_min = 5,
+        k_max = 20,
+        k_step = 5,
+    )],
+    uid = [''],
+    toposig_kwargs=[dict()],
+    method_args=[dict(n_jobs = 1, normalize = True, mu_push = 1.25)],
+    experiment_dir='/cluster/scratch/schsimo/output/WCTopoAE_swissroll_apush_stability',
+    seed = seed,
+    device = 'cpu',
+    num_threads=1,
+    verbose = False,
+) for seed in [574,  85, 136]]
