6b85275fb8876b577da12a566d10d05fab94aba0
MrBellamonte <schsimo@ethz.ch>
2020-10-08 23:43:47 +0200

    Euler experiments

--- scripts/ssc/wc_offline/config_libraries/euler/mnist.py
+++ scripts/ssc/wc_offline/config_libraries/euler/mnist.py
@@ -9,7 +9,7 @@ mnist_1 = ConfigWC_Grid(
     wc_kwargs=[dict()],
     eval_size=[0.14], # not a lot of data "wasted for bs = 4096 & could go to bs=8192
     n_jobs = [10],
-    seed = [838, 943, 811, 381, 267],
+    seed = [838],
     global_register = PATH_GR_MNIST_EULER,
     root_path = '/cluster/home/schsimo/MT/output/WitnessComplexes/mnist',
     verbose = True
