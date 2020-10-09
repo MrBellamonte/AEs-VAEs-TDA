9cd74d69da878d93fe4818d382fb9a5b9df0360f
MrBellamonte <schsimo@ethz.ch>
2020-10-09 12:41:02 +0200

    Euler experiments

--- scripts/ssc/wc_offline/config_libraries/euler/mnist.py
+++ scripts/ssc/wc_offline/config_libraries/euler/mnist.py
@@ -5,10 +5,10 @@ from src.datasets.datasets import SwissRoll, MNIST, MNIST_offline
 mnist_1 = ConfigWC_Grid(
     dataset = [MNIST_offline()],
     sampling_kwargs = [dict(root_path = '/cluster/home/schsimo/MT/AEs-VAEs-TDA')],
-    batch_size=[64,128,256,512,1024,2048,4096],
+    batch_size=[64,128,256,512,1024],
     wc_kwargs=[dict()],
     eval_size=[0.14], # not a lot of data "wasted for bs = 4096 & could go to bs=8192
-    n_jobs = [10],
+    n_jobs = [24],
     seed = [838],
     global_register = PATH_GR_MNIST_EULER,
     root_path = '/cluster/home/schsimo/MT/output/WitnessComplexes/mnist',
