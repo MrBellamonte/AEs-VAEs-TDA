b94890f15e10d48254201db2949a7eea5ee1d9b8
MrBellamonte <schsimo@ethz.ch>
2020-10-20 17:52:57 +0200

    Eval fix

--- src/models/WitnessComplexAE/train_engine.py
+++ src/models/WitnessComplexAE/train_engine.py
@@ -78,7 +78,7 @@ def train_TopoAE_ext(_run, _seed, _rnd, config: ConfigWCAE, experiment_dir, expe
     # Initialize model
     if X_train.shape[0]>10000:
         #todo fix somehow
-        inds = random.sample(range(X_train.shape[0]), 10000)
+        inds = random.sample(range(X_train.shape[0]), 4096)
         norm_X = torch.norm(dataset_train[inds][:][0][:, None]-dataset_train[inds][:][0], dim=2,
                             p=2).max()
     else:
