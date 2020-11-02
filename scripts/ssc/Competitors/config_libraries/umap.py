97b51f08b7c0eca89c8453499a49526e44fe8704
MrBellamonte <schsimo@ethz.ch>
2020-11-02 18:26:14 +0100

    Eval fix

--- scripts/ssc/Competitors/config_libraries/umap.py
+++ scripts/ssc/Competitors/config_libraries/umap.py
@@ -74,3 +74,28 @@ swissroll_euler = [ConfigGrid_Competitors(
     seed = seed,
     verbose = True
 ) for seed in [480, 367, 887, 718, 672, 172,  12, 326, 910, 688]]
+
+
+swissroll_euler_final = [ConfigGrid_Competitors(
+    model_class = [UMAP],
+    model_kwargs=dict(n_neighbors = [15,20,25,30,35,40,45,50,55,65,70], min_dist = [0.05,0.1,0.15,0.2,0.25]),
+    dataset=[SwissRoll()],
+    sampling_kwargs={
+        'n_samples': [2560]
+    },
+    eval=[ConfigEval(
+        active=True,
+        evaluate_on='test',
+        eval_manifold=True,
+        save_eval_latent=True,
+        save_train_latent=True,
+        online_visualization=False,
+        k_min=15,
+        k_max=45,
+        k_step=15,
+    )],
+    uid = [''],
+    experiment_dir='/cluster/scratch/schsimo/output/umap_final_final',
+    seed = seed,
+    verbose = True
+) for seed in [36, 3851, 2570, 4304, 1935, 7954, 5095, 5310, 1577, 3288]]
