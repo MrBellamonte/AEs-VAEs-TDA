29adef45098edcdc7f26a8e9b3693d6808a18b3e
MrBellamonte <schsimo@ethz.ch>
2020-11-06 12:51:20 +0100

    Eval fix

--- scripts/ssc/TopoAE_ext/debug_topoae2.py
+++ scripts/ssc/TopoAE_ext/debug_topoae2.py
@@ -1,9 +1,10 @@
+from scripts.ssc.TopoAE_ext.config_libraries.colab_configs.mnist import mnist_test512_cuda
 from scripts.ssc.TopoAE_ext.config_libraries.local_configs.mnist import mnist_test2, mnist_test3
 from scripts.ssc.TopoAE_ext.config_libraries.swissroll import debug
 from src.models.WitnessComplexAE.train_engine import simulator_TopoAE_ext
 
 if __name__ == "__main__":
 
-    configs = mnist_test3.configs_from_grid()
+    configs = mnist_test512_cuda.configs_from_grid()
     for config in configs:
         simulator_TopoAE_ext(config)
