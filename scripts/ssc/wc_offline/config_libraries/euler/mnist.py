4e7fef3aaded2ea973997c470b434e0c786add91
MrBellamonte <schsimo@ethz.ch>
2020-11-07 18:49:51 +0100

    Eval fix

--- scripts/ssc/wc_offline/config_libraries/euler/mnist.py
+++ scripts/ssc/wc_offline/config_libraries/euler/mnist.py
@@ -21,7 +21,7 @@ mnist_2 = ConfigWC_Grid(
     batch_size=[64,128,256,512,1024],
     wc_kwargs=[dict()],
     eval_size=[0.14], # not a lot of data "wasted for bs = 4096 & could go to bs=8192
-    n_jobs = [16],
+    n_jobs = [6],
     seed = [229],
     global_register = PATH_GR_MNIST_EULER,
     root_path = '/cluster/home/schsimo/MT/output/WitnessComplexes/mnist',
