59a929f1bb45d19a4359eb763deea0716ebe26cd
MrBellamonte <schsimo@ethz.ch>
2020-11-15 14:05:28 +0100

    Add tests

--- scripts/ssc/TopoAE_ext/config_libraries/euler_configs/unity.py
+++ scripts/ssc/TopoAE_ext/config_libraries/euler_configs/unity.py
@@ -185,5 +185,90 @@ leo_rotopenai_notopo = ConfigGrid_WCAE(
     verbose=False,
 )
 
+leo_rotopenai_decay2 = ConfigGrid_WCAE(
+    learning_rate=[1/1000, 1/100],
+    batch_size=[180],
+    n_epochs=[12000],
+    weight_decay=[0],
+    early_stopping=[125],
+    rec_loss_weight=[1],
+    top_loss_weight=[1],
+    match_edges=['push_active'],
+    k=[2],
+    r_max=[10],
+    model_class=[ConvAE_Unity480320],
+    model_kwargs=[dict()],
+    dataset=[Unity_RotOpenAI()],
+    sampling_kwargs=[dict(root_path = '/cluster/home/schsimo/MT/AEs-VAEs-TDA')],
+    eval=[ConfigEval(
+        active=True,
+        evaluate_on='test',
+        eval_manifold=False,
+        save_eval_latent=True,
+        save_train_latent=True,
+        online_visualization=False,
+        k_min=5,
+        k_max=10,
+        k_step=5,
+        quant_eval=False
+
+    )],
+    uid=[''],
+    toposig_kwargs=[dict()],
+    method_args=dict(n_jobs=[1], normalize=[True], mu_push=[1, 1.125], online_wc=[True],
+                     dist_x_land=[True],
+                     lam_t_decay=[
+                         {0  : 1024, 25: 512, 50: 256, 75: 128, 100: 64, 125: 32, 150: 16, 175: 8,
+                          200: 4, 250: 2, 300: 1, 350: 0.5, 400: 0.25, 450: 1/8, 500: 1/16, 550: 2**5, 600: 2**6, 650: 2**7,700: 2**8, 800: 2**10, 900: 2**12, 1000: 2**14, 1100: 2**16, 1200: 2**18},],
+                     wc_offline=[dict(path_to_data='/cluster/home/schsimo/MT/AEs-VAEs-TDA/src/datasets/simulated/openai_rotating')]),
+    experiment_dir='/cluster/scratch/schsimo/output/rotating_decay_2',
+    seed=2,
+    device='cuda',
+    num_threads=1,
+    verbose=False,
+)
+
+leo_rotopenai_decay3 = ConfigGrid_WCAE(
+    learning_rate=[1/1000, 1/100],
+    batch_size=[180],
+    n_epochs=[12000],
+    weight_decay=[0],
+    early_stopping=[125],
+    rec_loss_weight=[1],
+    top_loss_weight=[1],
+    match_edges=['push_active'],
+    k=[3],
+    r_max=[10],
+    model_class=[ConvAE_Unity480320],
+    model_kwargs=[dict()],
+    dataset=[Unity_RotOpenAI()],
+    sampling_kwargs=[dict(root_path = '/cluster/home/schsimo/MT/AEs-VAEs-TDA')],
+    eval=[ConfigEval(
+        active=True,
+        evaluate_on='test',
+        eval_manifold=False,
+        save_eval_latent=True,
+        save_train_latent=True,
+        online_visualization=False,
+        k_min=5,
+        k_max=10,
+        k_step=5,
+        quant_eval=False
+
+    )],
+    uid=[''],
+    toposig_kwargs=[dict()],
+    method_args=dict(n_jobs=[1], normalize=[True], mu_push=[1, 1.125], online_wc=[True],
+                     dist_x_land=[True],
+                     lam_t_decay=[
+                         {0  : 1024, 25: 512, 50: 256, 75: 128, 100: 64, 125: 32, 150: 16, 175: 8,
+                          200: 4, 250: 2, 300: 1, 350: 0.5, 400: 0.25, 450: 1/8, 500: 1/16, 550: 2**5, 600: 2**6, 650: 2**7,700: 2**8, 800: 2**10, 900: 2**12, 1000: 2**14, 1100: 2**16, 1200: 2**18},],
+                     wc_offline=[dict(path_to_data='/cluster/home/schsimo/MT/AEs-VAEs-TDA/src/datasets/simulated/openai_rotating')]),
+    experiment_dir='/cluster/scratch/schsimo/output/rotating_decay_2',
+    seed=2,
+    device='cuda',
+    num_threads=1,
+    verbose=False,
+)
 
 leo_rotopenai_1 = list(itertools.chain(*[config_grid.configs_from_grid() for config_grid in [leo_rotopenai_decay1,leo_rotopenai_notopo]]))
