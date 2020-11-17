14e5bd62ed3b32adcc6d5698daa096fc06e2d5a8
MrBellamonte <schsimo@ethz.ch>
2020-11-17 19:13:04 +0100

    Add tests

--- scripts/ssc/unity_datasets/reconstruction_analysis/rotating_box.py
+++ scripts/ssc/unity_datasets/reconstruction_analysis/rotating_box.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import torch
 import numpy as np
@@ -6,9 +7,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from torchvision.transforms import transforms
 
+from src.models.COREL.eval_engine import get_latentspace_representation
 from src.models.WitnessComplexAE.wc_ae import WitnessComplexAutoencoder
 from src.models.autoencoder.autoencoders import ConvAE_Unity480320
-
+from src.utils.plots import plot_2Dscatter
 
 if __name__ == "__main__":
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
@@ -29,10 +31,11 @@ if __name__ == "__main__":
     root_path_retrain = '/Users/simons/MT_data/sync/leonhard_sync_scratch/rotating_retrain'
     exp_retrain1 = 'Unity_RotOpenAI-seed1-ConvAE_Unity480320-default-lr1_1000-bs180-nep20000-rlw1-tlw1-mepush_active9_8-k2-rmax10-seed1-becee7e8'
     exp_retrain2 = 'Unity_RotOpenAI-seed1-ConvAE_Unity480320-default-lr1_1000-bs180-nep20000-rlw1-tlw1-mepush_active9_8-k2-rmax10-seed1-1c9a4a34'
+    exp_retrain3 = 'Unity_RotOpenAI-seed1-ConvAE_Unity480320-default-lr1_10000-bs180-nep20000-rlw1-tlw1-mepush_active9_8-k2-rmax10-seed1-a1266ff7'
 
 
     root_path = root_path_retrain
-    exp = exp_retrain2
+    exp = exp_retrain3
 
     path_source = os.path.join(root_path,exp)
 
@@ -50,13 +53,23 @@ if __name__ == "__main__":
 
 
     # get latents
-    latents = pd.read_csv(os.path.join(path_source, 'train_latents.csv'))
+    try:
+        latents = pd.read_csv(os.path.join(path_source, 'train_latents.csv'))
+        latents_temp = latents[0:1]
+        # get reconstructed images
+        labels = latents['labels']
+        latents_tensor = torch.tensor(latents[['0','1']].values)
+    except:
+        dataloarder_train = torch.load(
+            os.path.join('/Users/simons/PycharmProjects/MT-VAEs-TDA/src/datasets/simulated/openai_rotating', 'dataloader_train.pt'))
 
-    latents_temp = latents[0:1]
-    # get reconstructed images
-    labels = latents['labels']
-    latents_tensor = torch.tensor(latents[['0','1']].values)
-    print(latents_tensor.shape)
+        X_eval, Y_eval, Z_eval = get_latentspace_representation(model, dataloarder_train,
+                                                                device='cpu')
+
+        plot_2Dscatter(Z_eval, Y_eval, palette='hsv',
+                       path_to_save=os.path.join(path_source, '{}.pdf'.format('latentes_cyclic')),
+                       title=None, show=True)
+        latents_tensor = torch.tensor(Z_eval)
 
     x_hat = model.decode(latents_tensor.float())
 
