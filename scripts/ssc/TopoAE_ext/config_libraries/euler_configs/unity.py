2ee1849661f7d2c12448e769e208ba56ffc4fecd
MrBellamonte <schsimo@ethz.ch>
2020-11-23 08:40:59 +0100

    Fix main.py

--- scripts/ssc/TopoAE_ext/config_libraries/euler_configs/unity.py
+++ scripts/ssc/TopoAE_ext/config_libraries/euler_configs/unity.py
@@ -436,6 +436,48 @@ xy_trans_l_newpers_1 = ConfigGrid_WCAE(
     verbose=False,
 )
 
+
+xy_trans_l_newpers_s2 = ConfigGrid_WCAE(
+    learning_rate=[1/100],
+    batch_size=[200],
+    n_epochs=[5000],
+    weight_decay=[0],
+    early_stopping=[250],
+    rec_loss_weight=[1],
+    top_loss_weight=[8],
+    match_edges=['push_active'],
+    k=[4],
+    r_max=[10],
+    model_class=[ConvAE_Unity480320],
+    model_kwargs=[dict()],
+    dataset=[Unity_XYTransOpenAI(version = 'xy_trans_l_newpers')],
+    sampling_kwargs=[dict(root_path = '/cluster/home/schsimo/MT/AEs-VAEs-TDA')],
+    eval=[ConfigEval(
+        active=True,
+        evaluate_on='test',
+        eval_manifold=False,
+        save_eval_latent=True,
+        save_train_latent=True,
+        online_visualization=False,
+        k_min=5,
+        k_max=10,
+        k_step=5,
+        quant_eval=False
+
+    )],
+    uid=[''],
+    toposig_kwargs=[dict()],
+    method_args=dict(n_jobs=[1], normalize=[True], mu_push=[1.125], online_wc=[True],
+                     dist_x_land=[True],val_size = [0],
+                     wc_offline=[dict(path_to_data='/cluster/home/schsimo/MT/output/WitnessComplexes/unity/Unity_XYTransOpenAI-bs200-seed1-noiseNone-e9e1dc6e')]),
+    experiment_dir='/cluster/scratch/schsimo/output/xy_trans_l_newpers_1',
+    seed=2,
+    device='cuda',
+    num_threads=2,
+    verbose=False,
+)
+
+
 xy_trans_l_newpers_notopo = ConfigGrid_WCAE(
     learning_rate=[1/10,1/100,1/1000],
     batch_size=[400],
