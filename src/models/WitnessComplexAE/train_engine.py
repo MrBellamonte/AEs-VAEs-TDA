a8507cbbd6b87cd802dcff1bac2700113bdb6870
MrBellamonte <schsimo@ethz.ch>
2020-11-23 16:47:06 +0100

    Fix main.py

--- src/models/WitnessComplexAE/train_engine.py
+++ src/models/WitnessComplexAE/train_engine.py
@@ -14,7 +14,7 @@ from sacred.observers import FileStorageObserver
 from torch.utils.data import TensorDataset
 
 
-from scripts.ssc.TopoAE_ext.config_libraries.swissroll import swissroll_testing
+from scripts.ssc.models.TopoAE_ext.config_libraries.swissroll import swissroll_testing
 from src.datasets.datasets import Unity_Rotblock, Unity_RotCorgi, MNIST, MNIST_offline
 
 from src.models.WitnessComplexAE.config import ConfigGrid_WCAE, ConfigWCAE
