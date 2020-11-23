ecb7e2d9a368f2577c42bf6d475bd0ccd6becf5a
MrBellamonte <schsimo@ethz.ch>
2020-11-23 21:17:06 +0100

    Fix main.py

--- scripts/ssc/run_simulator_main_parallel.py
+++ scripts/ssc/run_simulator_main_parallel.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     args = parse_input()
 
     if args.model == 'topoae':
-        conifg_srt = 'scripts.ssc.TopoAE.config_libraries.'+args.configs
+        conifg_srt = 'scripts.ssc.models.TopoAE.config_libraries.'+args.configs
         mod_name, config_name = conifg_srt.rsplit('.', 1)
         mod = importlib.import_module(mod_name)
         configs = getattr(mod, config_name)
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
         Parallel(n_jobs=args.n_jobs)(delayed(simulator_TopoAE)(config) for config in configs)
     elif args.model == 'topoae_ext':
-        conifg_srt = 'scripts.ssc.TopoAE_ext.config_libraries.'+args.configs
+        conifg_srt = 'scripts.ssc.models.TopoAE_ext.config_libraries.'+args.configs
         mod_name, config_name = conifg_srt.rsplit('.', 1)
         mod = importlib.import_module(mod_name)
         configs = getattr(mod, config_name)
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         Parallel(n_jobs=args.n_jobs)(delayed(simulator_TopoAE_ext)(config) for config in configs)
 
     elif args.model == 'competitor':
-        conifg_srt = 'scripts.ssc.Competitors.config_libraries.'+args.configs
+        conifg_srt = 'scripts.ssc.models.Competitors.config_libraries.'+args.configs
         mod_name, config_name = conifg_srt.rsplit('.', 1)
         mod = importlib.import_module(mod_name)
         configs = getattr(mod, config_name)
