bc73f369f05b20fa6ab698fdaf7e79f0fad74018
MrBellamonte <schsimo@ethz.ch>
2020-11-23 12:35:45 +0100

    Fix main.py

--- scripts/ssc/wc_offline/run.py
+++ scripts/ssc/wc_offline/run.py
@@ -1,4 +1,4 @@
-from scripts.ssc.wc_offline.config_libraries.local.unity import unity_xytrans1, unity_xytrans_rot
+from scripts.ssc.wc_offline.config_libraries.euler.unity import unity_xytrans_rot
 from src.data_preprocessing.witness_complex_offline.compute_wc import compute_wc_multiple
 
 if __name__ == "__main__":
