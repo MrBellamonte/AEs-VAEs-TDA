b820971bfa04fadafbbbf46a603d5545bc311c82
MrBellamonte <schsimo@ethz.ch>
2020-12-08 12:06:40 +0100

    Implemented vanilla AE

--- scripts/ssc/models/Competitors/config_libraries/umap_mnist.py
+++ scripts/ssc/models/Competitors/config_libraries/umap_mnist.py
@@ -1,16 +1,19 @@
+import numpy as np
+
 from src.competitors.competitor_models import UMAP
 from src.competitors.config import ConfigGrid_Competitors
 from src.datasets.datasets import SwissRoll, MNIST_offline
 from src.evaluation.config import ConfigEval
 
+
 umap_mnist_euler_1 = ConfigGrid_Competitors(
     model_class = [UMAP],
-    model_kwargs=dict(n_neighbors = [15,20,25,30,35,40,45,50,55,65,70], min_dist = [0.05,0.1,0.15,0.2,0.25]),
+    model_kwargs=dict(test_eval=[False],n_neighbors = np.linspace(10,500,15).tolist(), min_dist = np.linspace(0.1,1,10).tolist()),
     dataset=[MNIST_offline()],
-    sampling_kwargs=[dict(root_path='/cluster/home/schsimo/MT/AEs-VAEs-TDA')],
+    sampling_kwargs=[dict(root_path='/cluster/home/schsimo/MT/AEs-VAEs-TDA', n_samples = 10000)],
     eval=[ConfigEval(
         active=True,
-        evaluate_on='test',
+        evaluate_on=None,
         eval_manifold=False,
         save_eval_latent=True,
         save_train_latent=False,
@@ -23,3 +26,29 @@ umap_mnist_euler_1 = ConfigGrid_Competitors(
     seed = 1,
     verbose = True
 )
+
+
+umap_mnist_test_local = ConfigGrid_Competitors(
+    model_class = [UMAP],
+    model_kwargs=dict(test_eval=[False],n_neighbors = [70], min_dist = [0.05]),
+    dataset=[MNIST_offline()],
+    sampling_kwargs={
+        'n_samples': [2560]
+    },
+    eval=[ConfigEval(
+        active=True,
+        evaluate_on=None,
+        eval_manifold=False,
+        save_eval_latent=True,
+        save_train_latent=True,
+        online_visualization=False,
+        k_min=4,
+        k_max=16,
+        k_step=4)],
+    uid = [''],
+    experiment_dir='/Users/simons/PycharmProjects/MT-VAEs-TDA/output/competitors/mnist_testing',
+    seed = 1,
+    verbose = True
+)
+
+
--- scripts/ssc/models/Competitors/test_competitors.py
+++ scripts/ssc/models/Competitors/test_competitors.py
@@ -1,8 +1,10 @@
-from scripts.ssc.models.Competitors import (
-    mnist_test)
+from scripts.ssc.models.Competitors.config_libraries.umap_mnist import (
+    umap_mnist_test_local,
+    umap_mnist_euler_1)
 from src.competitors.train_engine import simulator_competitor
 
 if __name__ == "__main__":
-    simulator_competitor(mnist_test)
+    for config in umap_mnist_test_local.configs_from_grid():
+        print(config)
 
 
