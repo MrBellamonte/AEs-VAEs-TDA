9386f73db18d3d2d1bf62564ba747e56ab4f7e7e
MrBellamonte <schsimo@ethz.ch>
2020-12-08 20:49:37 +0100

    Implemented vanilla AE

--- scripts/ssc/models/Competitors/config_libraries/umap_mnist.py
+++ scripts/ssc/models/Competitors/config_libraries/umap_mnist.py
@@ -6,7 +6,7 @@ from src.datasets.datasets import SwissRoll, MNIST_offline
 from src.evaluation.config import ConfigEval
 
 
-umap_mnist_euler_1 = ConfigGrid_Competitors(
+umap_mnist_euler_multi = [ConfigGrid_Competitors(
     model_class = [UMAP],
     model_kwargs=dict(test_eval=[False],n_neighbors = np.linspace(10,500,15).astype(int).tolist(), min_dist = np.linspace(0.1,1,10).tolist()),
     dataset=[MNIST_offline()],
@@ -16,16 +16,16 @@ umap_mnist_euler_1 = ConfigGrid_Competitors(
         evaluate_on=None,
         eval_manifold=False,
         save_eval_latent=True,
-        save_train_latent=False,
+        save_train_latent=True,
         online_visualization=False,
         k_min=4,
         k_max=16,
         k_step=4)],
     uid = [''],
-    experiment_dir='/cluster/scratch/schsimo/output/umap_mnist',
-    seed = 1,
+    experiment_dir='/cluster/scratch/schsimo/output/umap_mnist2',
+    seed = seed,
     verbose = True
-)
+) for seed in [1110,1909,1318]]
 
 
 umap_mnist_test_local = ConfigGrid_Competitors(
