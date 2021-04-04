df673f8531a51894db673ef6832d9583d391dd05
MrBellamonte <schsimo@ethz.ch>
2021-04-04 14:06:56 +0200

    Implemented vanilla AE

--- scripts/config_library/swissroll_vae.py
+++ scripts/config_library/swissroll_vae.py
@@ -190,6 +190,54 @@ vae_run1_seed36 = [ConfigGrid_WCAE(
     verbose=False,
 ) for seed, bs, dict_wc in zip([36]*len(bs), bs, SWISSROLL_NONOISE36)]
 
+
+vae_run1_seed36_remote = [ConfigGrid_WCAE(
+    learning_rate=[1/10],
+    batch_size=[bs],
+    n_epochs=[100],
+    weight_decay=[0],
+    early_stopping=[50],
+    rec_loss_weight=[1],
+    top_loss_weight=[8192],
+    match_edges=['push_active'],
+    k=[1],
+    r_max=[10],
+    model_class=[VanillaVAE],
+    model_kwargs={
+        'input_dim'         : [3],
+        'latent_dim'        : [2],
+        'size_hidden_layers': [[32, 32]],
+        'lambda_kld': list(np.logspace(-3,1,5))
+    },
+    dataset=[SwissRoll()],
+    sampling_kwargs={
+        'n_samples': [2560] #2560
+    },
+    eval=[ConfigEval(
+        active=True,
+        evaluate_on='test',
+        eval_manifold=True,
+        save_eval_latent=True,
+        save_train_latent=True,
+        online_visualization=False,
+        quant_eval=False,
+        k_min=5,
+        k_max=45,
+        k_step=5,
+    )],
+    uid=[''],
+    toposig_kwargs=[dict()],
+    method_args=dict(n_jobs=[1], normalize=[True], mu_push=[1], online_wc=[True], wc_offline = [dict_wc]),
+    experiment_dir='/home/simonberg/PycharmProjects/MT_contd/AEs-VAEs-TDA',
+    seed=36,
+    device='cpu',
+    num_threads=1,
+    verbose=False,
+) for seed, bs, dict_wc in zip([36]*len(bs), bs, SWISSROLL_NONOISE36)]
+
+
+
+
 vae_run1_seed36_list = list(itertools.chain(*[config_grid.configs_from_grid() for config_grid in vae_run1_seed36]))
 
 print(len(vae_run1_seed36_list))
