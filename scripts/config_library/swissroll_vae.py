949250fe91574729adfb16318797dff6cc3e9d7d
MrBellamonte <schsimo@ethz.ch>
2021-04-04 14:34:40 +0200

    Implemented vanilla AE

--- scripts/config_library/swissroll_vae.py
+++ scripts/config_library/swissroll_vae.py
@@ -4,7 +4,8 @@ import random
 import numpy as np
 
 from scripts.ssc.models.TopoAE_ext.config_libraries.local_configs.wc_offline_configs.swissroll import \
-    SWISSROLL_NONOISE36
+    (
+    SWISSROLL_NONOISE36, SWISSROLL_NONOISE36_LOCALSERVER)
 from src.datasets.datasets import SwissRoll
 from src.evaluation.config import ConfigEval
 from src.models.WitnessComplexAE.config import ConfigGrid_WCAE
@@ -59,49 +60,6 @@ vae_test = ConfigGrid_WCAE(
 bs = [64,128,256,512]
 
 
-
-
-# swissroll_h22 = [ConfigGrid_WCAE(
-#     learning_rate=[1/10, 1/100, 1/1000],
-#     batch_size=[int(bs)],
-#     n_epochs=[1000],
-#     weight_decay=[1e-6],
-#     early_stopping=[50],
-#     rec_loss_weight=[1],
-#     top_loss_weight=[int(i) for i in np.logspace(9, 13, num=5, base=2.0)],
-#     match_edges=['push_active'],
-#     k=[1,2,3,4,5,6],
-#     r_max=[10],
-#     model_class=[Autoencoder_MLP_topoae],
-#     model_kwargs={
-#         'input_dim'         : [3],
-#         'latent_dim'        : [2],
-#         'size_hidden_layers': [[32, 32]]
-#     },
-#     dataset=[SwissRoll()],
-#     sampling_kwargs={
-#         'n_samples': [2560]
-#     },
-#     eval=[ConfigEval(
-#         active=True,
-#         evaluate_on='test',
-#         eval_manifold=True,
-#         save_eval_latent=True,
-#         save_train_latent=True,
-#         online_visualization=False,
-#         k_min=15,
-#         k_max=45,
-#         k_step=15,
-#     )],
-#     uid=[''],
-#     toposig_kwargs=[dict()],
-#     method_args=dict(n_jobs=[1], normalize=[True], mu_push=[1,1.05,1.1,1.15,1.2,1.25], online_wc=[True], wc_offline = [dict_wc]),
-#     experiment_dir='/cluster/scratch/schsimo/output/WCAE_swissroll_nonoise_FINAL',
-#     seed=int(seed),
-#     device='cpu',
-#     num_threads=1,
-#     verbose=False,
-# ) for seed, bs, dict_wc in zip(36, bs, SWISSROLL_NONOISE36)]
 vae_run1_seed36 = [ConfigGrid_WCAE(
     learning_rate=[1/10,1/100,1/1000],
     batch_size=[bs],
@@ -228,12 +186,12 @@ vae_run1_seed36_remote = [ConfigGrid_WCAE(
     uid=[''],
     toposig_kwargs=[dict()],
     method_args=dict(n_jobs=[1], normalize=[True], mu_push=[1], online_wc=[True], wc_offline = [dict_wc]),
-    experiment_dir='/home/simonberg/PycharmProjects/MT_contd/AEs-VAEs-TDA',
+    experiment_dir='/home/simonberg/PycharmProjects/MT_contd/AEs-VAEs-TDA/output/WAE/vae_test',
     seed=36,
     device='cpu',
     num_threads=1,
     verbose=False,
-) for seed, bs, dict_wc in zip([36]*len(bs), bs, SWISSROLL_NONOISE36)]
+) for seed, bs, dict_wc in zip([36]*len(bs), bs, SWISSROLL_NONOISE36_LOCALSERVER)]
 
 
 
--- scripts/ssc/wc_offline/config_libraries/global_register_definitions.py
+++ scripts/ssc/wc_offline/config_libraries/global_register_definitions.py
@@ -9,8 +9,14 @@ PATH_GR_MNIST_EULER= '/cluster/home/schsimo/MT/output/WitnessComplexes/mnist/wc_
 
 PATH_GR_UNITY_EULER = '/cluster/home/schsimo/MT/output/WitnessComplexes/unity/wc_global_register_unity.csv'
 
+
+# PATH LOCAL SERVER
+PATH_GR_SWISSROLL_LOCAL_SERVER = '/Users/simons/PycharmProjects/MT-VAEs-TDA/output/WitnessComplex_offline/SwissRoll/wc_global_register_swissroll_local.csv'
+
+
+
 # LOCAL
-PATH_GR_SWISSROLL_LOCAL='/Users/simons/PycharmProjects/MT-VAEs-TDA/output/WitnessComplex_offline/SwissRoll/wc_global_register_swissroll_local.csv'
+PATH_GR_SWISSROLL_LOCAL='/home/simonberg/PycharmProjects/MT_contd/AEs-VAEs-TDA/output/WitnessComplex_offline/SwissRoll/wc_global_register_swissroll_local.csv'
 
 PATH_GR_MNIST_LOCAL = '/Users/simons/PycharmProjects/MT-VAEs-TDA/output/WitnessComplex_offline/mnist/wc_global_register_mnist_loc.csv'
 PATH_GR_UNITY_LOCAL = '/Users/simons/PycharmProjects/MT-VAEs-TDA/output/WitnessComplex_offline/unity/wc_global_register_unity.csv'
