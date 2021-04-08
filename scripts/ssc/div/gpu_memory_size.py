915dd2066ee0e3ced93005ef476a9353fa5bc6e1
MrBellamonte <schsimo@ethz.ch>
2021-04-08 11:06:50 +0200

    Implemented vanilla AE

---
+++ scripts/ssc/div/gpu_memory_size.py
@@ -0,0 +1,20 @@
+import argparse
+
+import torch
+
+def parse_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', "--path", type=str)
+    parser.add_argument('-bs', "--batch_size",
+                        default=64,
+                        help="number of parallel processes", type=int)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    args = parse_input()
+
+    dataset = torch.load('{}/full_dataset.pt'.format(args.path))
+
+    x = dataset[:args.batch_size][0].size().to('cuda')
