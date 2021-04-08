d91fba07a69fd117b7447c0d830b8b5f44a5910b
MrBellamonte <schsimo@ethz.ch>
2021-04-08 12:03:26 +0200

    Implemented vanilla AE

--- scripts/config_library/unity_trans_vae.py
+++ scripts/config_library/unity_trans_vae.py
@@ -4,6 +4,8 @@ from src.datasets.datasets import Unity_RotOpenAI, Unity_XYTransOpenAI
 from src.evaluation.config import ConfigEval
 from src.models.WitnessComplexAE.config import ConfigGrid_WCAE
 from src.models.autoencoder.autoencoders import ConvAE_Unity480320
+from src.models.vanillaAE.config import ConfigGrid_VanillaAE
+from src.models.variational_autoencoder.varautoencoders import ConvVAE_Unity480320
 
 xy_trans_final_1_k2 = ConfigGrid_WCAE(
     learning_rate=[1/100],
@@ -44,3 +46,35 @@ xy_trans_final_1_k2 = ConfigGrid_WCAE(
     num_threads=2,
     verbose=False,
 )
+
+
+xy_trans_test= ConfigGrid_VanillaAE(
+    learning_rate=[1/10],
+    batch_size=[441],
+    n_epochs=[10000],
+    weight_decay=[1e-6],
+    early_stopping=[1000],
+    model_class=[ConvVAE_Unity480320],
+    model_kwargs={'lambda_kld': [1]},
+    dataset=[Unity_XYTransOpenAI(version = 'xy_trans_final')],
+    sampling_kwargs=[dict(root_path='home/simonberg/PycharmProjects/MT_contd/AEs-VAEs-TDA')],
+    eval=[ConfigEval(
+        active=True,
+        evaluate_on='test',
+        eval_manifold=False,
+        save_eval_latent=True,
+        save_train_latent=True,
+        online_visualization=False,
+        k_min=5,
+        k_max=10,
+        k_step=5,
+        quant_eval=False
+
+    )],
+    uid=[''],
+    method_args=dict(val_size=[0]),
+    experiment_dir='/home/simonberg/PycharmProjects/MT_contd/AEs-VAEs-TDA/output/vanillaVAE/xy_trans_test',
+    seed=838,
+    device='cuda',
+    num_threads=2,
+    verbose=False)
--- src/models/variational_autoencoder/varautoencoders.py
+++ src/models/variational_autoencoder/varautoencoders.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+from src.models.autoencoder.utils import View
 from src.models.variational_autoencoder.base import VariationalAutoencoderModel
 
 
@@ -113,3 +114,138 @@ class VanillaVAE(VariationalAutoencoderModel):
 
     def decode(self, z):
         return self.decoder(z)
+
+
+class ConvVAE_Unity480320(VariationalAutoencoderModel):
+    '''
+    Implementation of a "standard autoencoder"
+    '''
+    __slots__ = ['encoder', 'decoder','reconst_error']
+
+    def __init__(self, input_dim: int, latent_dim: int, size_hidden_layers: list,lambda_kld = 1):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=4, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=4, out_channels=8, kernel_size=2, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=2, stride=2)
+        self.conv4 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, stride=2)
+        self.conv5 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=4)
+
+        self.conv5t = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=4)
+        self.conv4t = nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=2, stride=2)
+        self.conv3t = nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=2, stride=2)
+        self.conv2t = nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=2, stride=2)
+        self.conv1t = nn.ConvTranspose2d(in_channels=4, out_channels=3, kernel_size=1, stride=1)
+
+        self.encoder = nn.Sequential(
+            self.conv1,
+            nn.ReLU(True),
+            self.conv2,
+            nn.ReLU(True),
+            self.conv3,
+            nn.ReLU(True),
+            self.conv4,
+            nn.ReLU(True),
+            self.conv5,
+            nn.ReLU(True),
+            View((-1, 64*15*10)),
+            nn.Linear(64*15*10, 8),
+            nn.ReLU(True),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 64*15*10),
+            View((-1, 64, 10, 15)),
+            self.conv5t,
+            nn.ReLU(True),
+            self.conv4t,
+            nn.ReLU(True),
+            self.conv3t,
+            nn.ReLU(True),
+            self.conv2t,
+            nn.ReLU(True),
+            self.conv1t,
+            nn.Tanh()
+        )
+
+        self.fc_mu = nn.Linear(8, latent_dim)
+        self.fc_var = nn.Linear(8, latent_dim)
+
+        self.reconst_error = nn.MSELoss()
+        self.lambda_kld = lambda_kld
+
+
+    def encode(self, x):
+        result = self.encoder(x)
+        result = torch.flatten(result, start_dim=1)
+
+        mu = self.fc_mu(result)
+        log_var = self.fc_var(result)
+        return self.reparameterize(mu, log_var)
+
+    def encode_mustd(self,x):
+        result = self.encoder(x)
+        result = torch.flatten(result, start_dim=1)
+
+        mu = self.fc_mu(result)
+        log_var = self.fc_var(result)
+        return mu, log_var
+
+    def sample(self,
+               num_samples:int,
+               device):
+        """
+        Samples from the latent space and return the corresponding
+        image space map.
+        :param num_samples: (Int) Number of samples
+        :param current_device: (Int) Device to run the model
+        :return: (Tensor)
+        """
+        z = torch.randn(num_samples,
+                        self.latent_dim)
+
+        z = z.to(device)
+
+        samples = self.decode(z)
+        return samples
+
+    def forward(self,x):
+        """
+        Computes the VAE loss function.
+        KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+        mu, log_var = self.encode_mustd(x)
+        z = self.reparameterize(mu, log_var)
+
+        x_reconst = self.decode(z)
+
+
+        reconst_error = self.reconst_error(x, x_reconst)
+
+        kld_loss = torch.mean(-0.5*torch.sum(1+log_var-mu**2-log_var.exp(), dim=1), dim=0)
+
+        loss = reconst_error+self.lambda_kld*kld_loss
+        return loss, {'reconstruction_error': reconst_error, 'KLD': -kld_loss}
+
+    def reparameterize(self, mu, logvar):
+        """
+        Reparameterization trick to sample from N(mu, var) from
+        N(0,1).
+        :param mu: (Tensor) Mean of the latent Gaussian [B x D]
+        :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
+        :return: (Tensor) [B x D]
+        """
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+
+    def decode(self, z):
+        return self.decoder(z)
+
+
+
